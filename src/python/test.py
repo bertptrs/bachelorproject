2c5e10d73190922037dd508ae4a3b1f0fe841cae
Bert Peters <bert.ljpeters@gmail.com>
2015-04-24 12:20:56 +0200

    Add the option to only run a specific algorithm.

--- src/python/test.py
+++ src/python/test.py
@@ -11,6 +11,7 @@ def main():
     parser.add_argument('-t', '--sink', help="Sink to use in the graph. Default is random.")
     parser.add_argument('-v', '--verbose', action="store_true", help="Increase verbosity")
     parser.add_argument('files', type=argparse.FileType('r'), help="The data file to use.", nargs="+")
+    parser.add_argument('-a', '--algorithm', help="The algorithm to use. Default is all of them")
 
     opts = parser.parse_args()
 
@@ -32,9 +33,16 @@ def main():
         print "Calculating flow over", maxflow.Edge(source, sink)
 
         graphs = maxflow.getGraphs()
+        algos = 0
         for graph in graphs:
             maxflow.initGraph(graph, edges)
-            print graph.__class__.__name__, ":", graph.run(source, sink)
+            if opts.algorithm is None or str(opts.algorithm) == str(graph.__class__.__name__):
+                print graph.__class__.__name__, ":", graph.run(source, sink)
+                algos = algos + 1
+
+        if algos is 0:
+            print "No known algorithm selected."
+            sys.exit(2)
 
 if __name__ == "__main__":
     main()
