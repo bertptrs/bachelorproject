aad5ffcb0515a7e3e7d655afbaa10c024a4a7830
Bert Peters <bert.ljpeters@gmail.com>
2015-12-18 16:13:06 +0100

    Remove runtime limit from MPI test runner.

--- mpi/create-tests.sh
+++ mpi/create-tests.sh
@@ -21,7 +21,6 @@ for COUNT in $WORKERCOUNTS; do
 	cat << EOF > "$filename"
 #!/bin/bash
 #$ -pe openmpi ${COUNT}
-#$ -l h_rt=0:15:00
 #$ -N PUSHLIFT_IMPL${IMPLEMENTATION}_${SOURCE}_${SINK}_${COUNT}
 #$ -cwd
 
