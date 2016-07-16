b9aa2059458119a390b3fa0361017a8a3913a87d
Bert Peters <bert.ljpeters@gmail.com>
2016-07-16 15:25:46 +0200

    Add a simple script to filter out useful results.

---
+++ mpi/filter-results.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Temporary file to store all files we want to delete.
+toDelete=$(mktemp)
+
+# Remove all files that do not contain the max flow result.
+grep -v "Max flow is" -l $@ > "$toDelete"
+
+# Remove all the files with a max flow of zero.
+grep "Max flow is 0\\\$" -l $@ > "$toDelete"
+
+# Remove the files.
+sort -u "$toDelete" | xargs rm
+
+# Remove the temporary.
+rm $toDelete
