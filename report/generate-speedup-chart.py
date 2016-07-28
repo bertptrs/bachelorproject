4b5806388a7d9f96d80238c49e56b98896b7f83e
Bert Peters <bert.ljpeters@gmail.com>
2016-07-28 16:01:08 +0200

    Remove build directory.

--- report/generate-speedup-chart.py
+++ report/generate-speedup-chart.py
@@ -1,45 +1,41 @@
-#!/usr/bin/python2
+#!/usr/bin/env python3
 
 import matplotlib.pyplot as plt
 import graphhelper
 
 def plot(timings):
-	avgs = []
-	coreList = timings.keys()
-	coreList.sort()
-	for coreNo in coreList:
-		avgs.append(sum(timings[coreNo]) / len(timings[coreNo]))
+    coreList = sorted(timings.keys())
 
-	speedups = []
-	for i in avgs:
-		speedups.append(avgs[0] / i)
+    avgs = [sum(timings[coreNo]) / len(timings[coreNo]) for coreNo in coreList]
 
-	plt.plot(coreList, speedups, label="Measured speedup")
-	plt.plot(coreList, coreList, label="Ideal speedup") # Plot the reference line
-	plt.xlabel("Number of MPI workers")
-	plt.ylabel("Relative speedup")
-	plt.legend(loc = "best", fancybox = True)
-	plt.grid()
+    speedups = [avgs[0] / avg for avg in avgs]
+
+    plt.plot(coreList, speedups, label="Measured speedup")
+    plt.plot(coreList, coreList, label="Ideal speedup") # Plot the reference line
+    plt.xlabel("Number of MPI workers")
+    plt.ylabel("Relative speedup")
+    plt.legend(loc = "best", fancybox = True)
+    plt.grid()
 
 def main():
-	parser = graphhelper.getArgParser()
-	args = parser.parse_args()
+    parser = graphhelper.getArgParser()
+    args = parser.parse_args()
 
-	timings = dict()
+    timings = dict()
 
-	for current in args.files:
-		ncores, initTime, algoTime, implementation = graphhelper.getData(current)
-		if ncores not in timings:
-			timings[ncores] = []
+    for current in args.files:
+        ncores, initTime, algoTime, implementation = graphhelper.getData(current)
+        if ncores not in timings:
+            timings[ncores] = []
 
-		timings[ncores].append(algoTime)
+        timings[ncores].append(algoTime)
 
-	plot(timings)
+    plot(timings)
 
-	if args.output:
-		plt.savefig(args.output.name)
-	else:
-		plt.show()
+    if args.output:
+        plt.savefig(args.output.name)
+    else:
+        plt.show()
 
 if __name__ == "__main__":
-	main()
+    main()
