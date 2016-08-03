febc106f2a0ff9d4a1c9abf9e5fae6037e007a2d
Bert Peters <bert.ljpeters@gmail.com>
2016-08-03 12:43:48 +0200

    Improve speedup graph in report.
    Speedup graph in report now contains all of the datasets rather than
    just one.

--- report/generate-speedup-chart.py
+++ report/generate-speedup-chart.py
@@ -2,15 +2,17 @@
 
 import matplotlib.pyplot as plt
 import graphhelper
+import tarfile
+import os.path as path
 
-def plot(timings):
+def plot(timings, label):
     coreList = sorted(timings.keys())
 
     avgs = [sum(timings[coreNo]) / len(timings[coreNo]) for coreNo in coreList]
 
     speedups = [avgs[0] / avg for avg in avgs]
 
-    plt.semilogx(coreList, speedups, label="Measured speedup", basex=2)
+    plt.semilogx(coreList, speedups, label=label, basex=2)
     plt.xlabel("Number of MPI workers")
     plt.ylabel("Relative speedup")
     plt.legend(loc = "best", fancybox = True)
@@ -20,16 +22,25 @@ def main():
     parser = graphhelper.getArgParser()
     args = parser.parse_args()
 
-    timings = dict()
 
-    for current in graphhelper.getArchiveIterator(args.files):
-        ncores, initTime, algoTime, implementation = graphhelper.getData(current)
-        if ncores not in timings:
-            timings[ncores] = []
+    for current in args.files:
+        timings = {}
+        name = path.basename(current.name)
+        label = name[:name.find(".")]
 
-        timings[ncores].append(algoTime)
+        archive = tarfile.open(fileobj=current);
 
-    plot(timings)
+        for tarinfo in archive:
+            datafile = archive.extractfile(tarinfo)
+
+            ncores, initTime, algoTime, implementation = graphhelper.getData(datafile)
+            if ncores not in timings:
+                timings[ncores] = []
+
+            timings[ncores].append(algoTime)
+
+        plot(timings, label)
+
 
     if args.output:
         plt.savefig(args.output.name)
